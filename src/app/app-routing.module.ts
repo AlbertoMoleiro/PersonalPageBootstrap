88d72bbbc145366e26d30699cf1938a1383d46b6
AlbertoMoleiro <alberto.moleiro@softtek.com>
2023-04-20 21:59:44 +0200

    Nav, Home y Contact terminadas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { BlogComponent } from './pages/blog/blog.component';
+import { ArticleComponent } from './pages/article/article.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'blog', component: BlogComponent },
+    { path: 'contact', component: ContactComponent },
+    { path: 'article/:id', component: ArticleComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
